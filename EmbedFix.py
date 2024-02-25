74d7440e217b6028dffc7e79e00ad3e33d71318d
Mac_cy Goat <Zitrone131@outlook.de>
2024-02-25 19:16:05 +0100

    Changed config.json structure, added multiple commands

--- EmbedFix.py
+++ EmbedFix.py
@@ -1,22 +1,52 @@
 
-import discord, requests, re
+import discord, discord.ui, requests, re, json
+from discord.ext import commands
 
 
+# open and parse config.json
 
-class __main__(discord.Client, discord.TextChannel):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
-
+with open('config.json', 'r') as f:
+    config = json.load(f)
+config_token_value = config['Token']
+only_react_in_specific_channels = config['OnlySpecificChannels']
+allowed_channels = config['AllowedChannels']
+instagram_prefix = config['Instagram']
+twitter_prefix = config['Twitter']
+tiktok_prefix = config['TikTok']
+# print(config_token_value)
+print("Instagram prefixes", instagram_prefix)
+print("Twitter prefixes", twitter_prefix)
+print("TikTok prefixes", tiktok_prefix)
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = __main__(intents=intents)
+bot = discord.Bot(intents=intents)
 
-tokenfile = open('config.json')
-tokenfilecontents = tokenfile.read()
-print(tokenfilecontents)
-client.run(tokenfilecontents)
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is ready and online!")
+
+@bot.event
+async def on_message(message):
+    print(f'Message from {message.author}: {message.content}')
+    if only_react_in_specific_channels is True:
+        if str(message.channel.id) in allowed_channels:
+            pass
+        else:
+            return
+    if only_react_in_specific_channels is False:
+        pass
+    if 'test' in message.content.lower():
+        embed = discord.Embed(title=f'test',description=f'**test** : ``test``', color=0x001fff )
+        await message.reply(embed=embed, mention_author=False)
+
+@bot.command(description="Sends the bot's latency.") # this decorator makes a slash command
+async def ping(ctx): # a slash command will be created with the name "ping"
+    await ctx.respond(f"Pong! Latency is {bot.latency}")
+
+@bot.command(description="Shows the bot source.") # this decorator makes a slash command
+async def source(ctx): # a slash command will be created with the name "ping"
+    await ctx.respond(f"You can find the source code [here](https://github.com/Maccygoat/EmbedFix)", ephemeral=True)
+
+bot.run(config_token_value)
--- README.md
+++ README.md
@@ -4,7 +4,30 @@ Add a config.json file to your installation folder.
 ```
 {
   "Token": "Token",
-  "Prefix": [ "https://www.instagram.com/", "https://instagram.com/", "http://www.instagram.com/", "http://instagram.com/", "https://www.twitter.com/", "https://twitter.com/", "http://www.twitter.com/", "http://twitter.com/", "https://www.x.com/", "https://x.com/", "http://www.x.com/", "http://x.com/", "https://www.tiktok.com/", "https://tiktok.com/", "http://www.tiktok.com/", "http://tiktok.com/" ],
+  "Instagram": [
+    "https://www.instagram.com/",
+    "https://instagram.com/",
+    "http://www.instagram.com/",
+    "http://instagram.com/"
+  ],
+  "Twitter": [
+    "https://www.twitter.com/",
+    "https://twitter.com/",
+    "http://www.twitter.com/",
+    "http://twitter.com/",
+    "https://www.x.com/",
+    "https://x.com/",
+    "http://www.x.com/",
+    "http://x.com/"
+  ],
+  "TikTok": [
+    "https://www.tiktok.com/",
+    "https://tiktok.com/",
+    "http://www.tiktok.com/",
+    "http://tiktok.com/"
+  ],
+  "OnlySpecificChannels": true/false,
+  "AllowedChannels": [ "ID", "ID" ],
   "OwnerID": "ID",
   "TestGuildID": "ID",
   "DMErrors": "true/false",
