7890128e901787b865be2e9649182d99a99b9484
Mac_cy Goat <Zitrone131@outlook.de>
2024-02-25 16:07:20 +0100

    Test

--- EmbedFix.py
+++ EmbedFix.py
@@ -1,2 +1,22 @@
 
+import discord, requests, re
 
+
+
+class __main__(discord.Client, discord.TextChannel):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = __main__(intents=intents)
+
+tokenfile = open('config.json')
+tokenfilecontents = tokenfile.read()
+print(tokenfilecontents)
+client.run(tokenfilecontents)
